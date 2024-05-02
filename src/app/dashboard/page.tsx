7411db436403998f3ea9b37f28afa71d9e9dc694
Unknown Person <104310243+unknownpersonog@users.noreply.github.com>
2024-05-02 05:46:42 +0000

    removed unused import

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 import Dashboard from "@/components/dashboard";
-import Loading from "@/components/loading";
-import { useUser } from '@auth0/nextjs-auth0/client';
 export default function Home() {
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 
-import { useUser } from '@auth0/nextjs-auth0/client';
 import Dashboard from '@/components/dashboard';
-import LoadingComponent from '@/components/loading';
 import Login from '@/components/login';
 import { redirect } from 'next/navigation';
 import { auth } from '../../auth';
-import { SessionProvider } from 'next-auth/react';
 
 export default async function Index() {
   const session = await auth();
