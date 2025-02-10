f806944c84f7938d21cb995842b74b640cf6ba1c
Unknown Person <104310243+unknownpersonog@users.noreply.github.com>
2025-02-10 14:42:05 +0000

    Remove redundant text from UserTable component

--- src/components/admin/UserTable.tsx
+++ src/components/admin/UserTable.tsx
@@ -135,7 +135,6 @@ const UserTable: React.FC<UserTableProps> = ({
                         alt="Google"
                         className="w-5 h-5 mr-2"
                       />
-                      Google
                     </div>
                   ) : (
                     user.method
